a33f224616cae1155c036eaaf81407043d575443
fendiprince <moneabraams@gmail.com>
2018-02-15 15:04:09 +0700

    sec proj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Hi!");
+            Console.WriteLine("Hi!");
             Console.ReadKey();
         }
 
